d88f7e9e3135610c93b5505687fa4a04991c465d
Luiz Lima <luiz.lima@quintoandar.com.br>
2022-02-23 10:37:39 -0300

    Add phrase parameter

--- app.ts
+++ app.ts
@@ -1,10 +1,10 @@
-function add(n1: number, n2: number, showResult: boolean) {
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
   // if (typeof n1 !== "number" || typeof n2 !== "number") {
   //   throw new Error("Incorrect input!");
   // }
   const result = n1 + n2;
   if (showResult) {
-    console.log(result);
+    console.log(phrase + result);
   } else {
     return result;
   }
@@ -13,5 +13,6 @@ function add(n1: number, n2: number, showResult: boolean) {
 const number1 = 5;
 const number2 = 2.8;
 const printResult = true;
+const resultPhrase = "Result is: ";
 
-add(number1, number2, printResult);
+add(number1, number2, printResult, resultPhrase);
