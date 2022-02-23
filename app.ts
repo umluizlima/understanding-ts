0e6f1f87fb20b962343b88294c8c730de44dc7b9
Luiz Lima <luiz.lima@quintoandar.com.br>
2022-02-23 10:35:27 -0300

    Add showResult boolean parameter

--- app.ts
+++ app.ts
@@ -1,13 +1,17 @@
-function add(n1: number, n2: number) {
+function add(n1: number, n2: number, showResult: boolean) {
   // if (typeof n1 !== "number" || typeof n2 !== "number") {
   //   throw new Error("Incorrect input!");
   // }
-  return n1 + n2;
+  const result = n1 + n2;
+  if (showResult) {
+    console.log(result);
+  } else {
+    return result;
+  }
 }
 
 const number1 = 5;
 const number2 = 2.8;
+const printResult = true;
 
-const result = add(number1, number2);
-
-console.log(result);
+add(number1, number2, printResult);
