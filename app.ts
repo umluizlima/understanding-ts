7867d2ef60d0bdb206665a5c0ae8d940313d7bc0
Luiz Lima <luiz.lima@quintoandar.com.br>
2022-02-27 18:01:25 -0300

    Add tuple

--- app.ts
+++ app.ts
@@ -1,12 +1,17 @@
-// const person: {
-//   name: string;
-//   age: number;
-// } = {
-const person = {
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
   name: "Luiz",
   age: 28,
   hobbies: ["Sports", "Cooking"],
+  role: [2, "author"],
 };
+
+person.role.push("admin");
+person.role[1] = 10;
 
 let favoriteActivities: any[];
 favoriteActivities = ["Sports", 1];
