40605e41cdc036dd99bfc3854e6c650fc25fae21
Luiz Lima <luiz.lima@quintoandar.com.br>
2022-02-27 18:46:43 -0300

    Add never type

--- app.ts
+++ app.ts
@@ -6,3 +6,10 @@ userInput = "Max";
 if (typeof userInput === "string") {
   userName = userInput;
 }
+
+function generateError(message: string, code: number): never {
+  throw { message, errorCode: code };
+  //   while (true) {}
+}
+
+generateError("An error occurred!", 500);
