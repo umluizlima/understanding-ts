2fcc395680348847c38f56e75d2ec30310742d27
Luiz Lima <luiz.lima@quintoandar.com.br>
2022-02-27 18:23:30 -0300

    Add literal type

--- app.ts
+++ app.ts
@@ -1,12 +1,22 @@
-function combine(input1: number | string, input2: number | string) {
-  if (typeof input1 === "number" && typeof input2 === "number") {
-    return input1 + input2;
+function combine(
+  input1: number | string,
+  input2: number | string,
+  resultConversion: "as-number" | "as-text"
+) {
+  if (
+    (typeof input1 === "number" && typeof input2 === "number") ||
+    resultConversion === "as-number"
+  ) {
+    return +input1 + +input2;
   } else {
     return input1.toString() + input2.toString();
   }
 }
 
-const combinedAges = combine(30, 26);
+const combinedAges = combine(30, 26, "as-number");
 console.log(combinedAges);
 
-const combinedNames = combine("Max", "Anna");
+const combinedStringAges = combine("30", "26", "as-number");
+console.log(combinedStringAges);
+
+const combinedNames = combine("Max", "Anna", "as-text");
