b55927f2f6909d1796ff7d908b5cb85caad3d053
Luiz Lima <luiz.lima@quintoandar.com.br>
2022-02-27 17:55:28 -0300

    Add array

--- app.ts
+++ app.ts
@@ -5,6 +5,10 @@
 const person = {
   name: "Luiz",
   age: 28,
+  hobbies: ["Sports", "Cooking"],
 };
+
+let favoriteActivities: any[];
+favoriteActivities = ["Sports", 1];
 
 console.log(person.name);
